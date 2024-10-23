6c1820348a0010f6ff92385bf468c94f04b85e1b
marq <classicvector63@gmail.com>
2024-10-23 14:10:01 +0530

    Implemented hardhat add test network SepoliaETH

--- tokencoin/hardhat.config.js
+++ tokencoin/hardhat.config.js
@@ -1,36 +1,9 @@
 require("@matterlabs/hardhat-zksync-solc");
+require("dotenv").config(); // Load .env variables
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  zksolc: {
-    version: "1.3.9",
-    compilerSource: "binary",
-    settings: {
-      optimizer: {
-        enabled: true,
-      },
-    },
-  },
-  networks: {
-    zksync_testnet: {
-      url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli",
-      chainId: 280,
-      zksync: true,
-    },
-    zksync_mainnet: {
-      url: "https://zksync2-mainnet.zksync.io/",
-      ethNetwork: "mainnet",
-      chainId: 324,
-      zksync: true,
-    },
-  },
-  paths: {
-    artifacts: "./artifacts-zk",
-    cache: "./cache-zk",
-    sources: "./contracts",
-    tests: "./test",
-  },
+  defaultNetwork: 'sepolia',
   solidity: {
     version: "0.8.17",
     settings: {
@@ -38,6 +11,13 @@ module.exports = {
         enabled: true,
         runs: 200,
       },
+    },
+  },
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: 'https://rpc.ankr.com/eth_sepolia',
+      accounts: process.env.PRIVATE_KEY ? [`0x${process.env.PRIVATE_KEY}`] : [],
     },
   },
 };
