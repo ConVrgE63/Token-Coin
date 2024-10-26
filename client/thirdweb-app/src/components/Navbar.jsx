bf9326a0f8c3f53e76d4e2deb991cc78b6e0feea
marq <classicvector63@gmail.com>
2024-10-26 15:34:16 +0530

    Navbar & Custom-Button

--- client/thirdweb-app/src/components/Navbar.jsx
+++ client/thirdweb-app/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
       <div className="lg:flex-1 flex flex-row max-w-[470px] py-2 pl-4 pr-2 h-[48px] bg-[#1c1c24] rounded-[100px]">
         <input type="text" placeholder="Search for campaigns" className="flex w-full font-epilogue font-normal text-[16px] placeholder:text-[#4b5264] text-white bg-transparent outline-none" />
         
-        <div className="w-[60px] h-full rounded-[20px] bg-[#35d0d0] flex justify-center items-center cursor-pointer">
+        <div className="w-[60px] h-full rounded-[20px] bg-[#3498db] flex justify-center items-center cursor-pointer">
           <img src={search} alt="search" className="w-[15px] h-[15px] object-contain"/>
         </div>
       </div>
@@ -26,7 +26,8 @@ const Navbar = () => {
         <CustomButton 
           btnType="button"
           title={address ? 'Create a campaign' : 'Connect'}
-          styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
+          styles={address ? 'bg-[#3498db]' : 'bg-[#3498db]'}
+
           handleClick={() => {
             if(address) navigate('create-campaign')
             else connect()
@@ -34,8 +35,8 @@ const Navbar = () => {
         />
 
         <Link to="/profile">
-          <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer">
-            <img src={thirdweb} alt="user" className="w-[60%] h-[60%] object-contain" />
+          <div className="w-[45px] h-[45px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+            <img src={thirdweb} alt="user" className="w-[50%] h-[50%] object-contain" />
           </div>
         </Link>
       </div>
@@ -70,7 +71,7 @@ const Navbar = () => {
                     alt={link.name}
                     className={`w-[24px] h-[24px] object-contain ${isActive === link.name ? 'grayscale-0' : 'grayscale'}`}
                   />
-                  <p className={`ml-[20px] font-epilogue font-semibold text-[14px] ${isActive === link.name ? 'text-[#1dc071]' : 'text-[#808191]'}`}>
+                  <p className={`ml-[20px] font-epilogue font-semibold text-[14px] ${isActive === link.name ? 'text-[#3498db]' : 'text-[#808191]'}`}>
                     {link.name}
                   </p>
                 </li>
@@ -81,7 +82,7 @@ const Navbar = () => {
             <CustomButton 
               btnType="button"
               title={address ? 'Create a campaign' : 'Connect'}
-              styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
+              styles={address ? 'bg-[#3498db]' : 'bg-[#3498db]'}
               handleClick={() => {
                 if(address) navigate('create-campaign')
                 else connect();
